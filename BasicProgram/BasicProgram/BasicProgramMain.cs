cc7eabb1e653567fce2c8ac38dba0cdc8128315e
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-21 14:32:17 +0530

    [Suraj]Add:Flip Coin

---
+++ BasicProgram/BasicProgram/BasicProgramMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class BasicProgramMain
+    {
+        public void FlipCoin()
+        {
+            int heads = 0;
+            int tails = 0;
+            int counter = 0;
+            Random coinflip = new Random();
+            Console.WriteLine("How many times would you like to flip a coin? ");
+            counter = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < counter; i++)
+            {
+                int flip = coinflip.Next(1, 3);
+                if (flip == 1)
+                {
+                    heads++;
+                }
+                else
+                {
+                    tails++;
+                }
+            }
+            Console.WriteLine("You flipped a coin " + counter
+               + " times " + "and you got " + heads + " heads and " + tails + " tails.");
+            
+        }        
+    }
+}
---
+++ BasicProgram/BasicProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BasicProgram
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Core Program.");
+            while(true)
+            {
+                Console.WriteLine("Select Option\n 1.FlipCoin");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        BasicProgramMain main = new BasicProgramMain();
+                        main.FlipCoin();
+                        break;
+
+                }
+            }
+        }
+    }
+}
