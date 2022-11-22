9b637a4dd5866e09e8230ea3de71a943dbc86a3d
Surajdhabale60 <112876188+Surajdhabale60@users.noreply.github.com>
2022-11-22 21:17:34 +0530

    "[Suraj]Add:Quotient And Reminder

--- BasicProgram/BasicProgram/BasicProgramMain.cs
+++ BasicProgram/BasicProgram/BasicProgramMain.cs
@@ -105,5 +105,16 @@ namespace BasicProgram
             else
                 Console.WriteLine("Entered Number is odd number");
         }
+        public void QuotientandReminder()
+        {
+            Console.WriteLine("Enter Dividend");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divisor");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+            int quotient = (dividend / divisor);
+            int reminder = (dividend % divisor);
+            Console.WriteLine("Quotient is  " + quotient);
+            Console.WriteLine("Remainder is " + reminder);
+        }
     }
 }
